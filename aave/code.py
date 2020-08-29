f8e5cfc90f4a681cb728fa340f6eebe804a73bce
Evert <evert@hatching.io>
2020-08-29 11:22:13 +0200

    Create overview command

--- aave/code.py
+++ aave/code.py
@@ -1,7 +1,50 @@
+import os
 import settings
 from web3 import Web3
+import datetime
+import json
 
-def do(address, human=False):
+with open(os.path.join("aave", "atokens.json")) as f:
+    atokens = json.load(f)
+
+with open(os.path.join("aave", "abi", "AToken.json")) as json_data:
+    aTokenAbi = json.load(json_data)
+
+def get_user_balance(user, human=False):
+    data = {}
+    # if os.path.exists("user.json"):
+    #     with open("temp.json", "r") as f:
+    #         return json.load(f)
+    for ticker, contracts in atokens.items():
+        if not contracts.get("aave"):
+            continue
+
+        reserveData = settings.CONTRACT_LP.functions.getUserReserveData(contracts["regular"], user).call()
+        print(reserveData)
+        aBalance = reserveData[0]
+        liquidityRate = reserveData[5]
+
+        if ticker == "ETH":
+            balance = settings.ENDPOINT.eth.getBalance(user)
+        else:
+            contract = settings.ENDPOINT.eth.contract(address=contracts["regular"], abi=aTokenAbi)
+            balance = contract.functions.balanceOf(user).call()
+
+        if human:
+            aBalance = round(float(Web3.fromWei(aBalance, "ether")), 2)
+            balance = round(float(Web3.fromWei(balance, "ether")), 2)
+            liquidityRate = round(float(liquidityRate/10000000000000000000000000), 2)
+
+        data[ticker] = {
+            "balance": balance,
+            "aBalance": aBalance,
+            "APY": liquidityRate
+        }
+    # with open("user.json", "w+") as f:
+    #     json.dump(data, f, indent=4)
+    return data
+
+def get_user_account_data(address, human=False):
     data = settings.CONTRACT_LP_WS.functions.getUserAccountData(address).call()
     rt = {
         "totalLiquidityETH": data[0],
--- tg.py
+++ tg.py
@@ -3,7 +3,7 @@ import logg
 import settings
 import threading
 import time
-from aave.code import do
+import aave.code as aave
 
 import traceback
 def error(update, error):
@@ -16,10 +16,35 @@ def handle_inline_result(update, context):
     query.answer()
     query.edit_message_text(text="Selected option:s".format(query.data))
 
+
 def handle_update_message(update, context):
     address = update.effective_user.address
-    data = do(address, human=True)
-    update.message.reply_text("Hello your current health factor is %s" % data["healthFactor"])
+    if update.message.text == "/start":
+        update.message.reply_text("Hi, this is the unofficial Aave bot. Manage your Aave positions and receive interesting update.")
+        if address:
+            update.message.reply_text("Your current address is <b>%s</b>, do you want to get an overview of your positions? press /overview" % address, parse_mode="HTML")
+        else:
+            update.message.reply_text("You did not link your Ethereum address to your telegram account yet,"
+            "please download the Totality fork or get in contact with @custodialbot")
+        return
+
+    if not address:
+        update.message.reply_text("You did not link your Ethereum address to your telegram account yet,"
+            "please download the Totality fork or get in contact with @custodialbot")
+        return
+
+    if update.message.text == "/overview":
+        user_account = aave.get_user_account_data(address, human=True)
+        balance = aave.get_user_balance(address, human=True)
+        msg = "<b>Coin</b>\taToken/Total\n"
+        for ticker, balances in balance.items():
+            if balances["balance"] == 0 and balances["aBalance"] == 0:
+                continue
+            t = "<b>%s</b>\t<code>%s/%s</code> %s%% APY\n" % (ticker, balances["aBalance"],
+                round(balances["aBalance"] + balances["balance"], 2),
+                balances["APY"])
+            msg += t
+        update.message.reply_text("Healthfactor: <b>%s</b>\n\n%s" % (user_account["healthFactor"], msg), parse_mode="HTML")
 
 def main():
     """Start the bot."""
@@ -35,7 +60,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.command, handle_update_message))
     dp.add_handler(CallbackQueryHandler(handle_inline_result))
     # log all errors
-    dp.add_error_handler(error)
+    #dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
